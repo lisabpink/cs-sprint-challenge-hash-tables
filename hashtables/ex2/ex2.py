a8d3ddcbed192fc66168d0458d55eb160e464722
LISA_COOK <lisabpink@gmail.com>
2020-07-10 12:02:43 -0400

    ex2, One-Way Flight Trip

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -8,7 +8,27 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
+    Key: starting location
+    Value: destination
     """
     # Your code here
+    flight = dict()
+    route = []
+
+    for i in range(length):
+        key = tickets[i].source
+        value = tickets[i].destination
+
+        flight[key] = value
+
+    # If source = "NONE", is first ticket
+    key = flight["NONE"]
+
+    # If source is not first ticket
+    while key != "NONE":
+        route.append(key)
+
+        key = flight[key]
+    route.append(key)
 
     return route
