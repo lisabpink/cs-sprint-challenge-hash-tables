cad5b5438e9dfa5cd8289dd1d89f1c7bb290ec96
LISA_COOK <lisabpink@gmail.com>
2020-07-10 11:46:19 -0400

    ex1,  Merging Two Packages

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,25 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    """
+    picked_weight & pairing_weight
+    picked_weight = key
+    pairing_weight = limit - picked_weight
+    if picked_weight >, put in zero index.
+    else put in first index.
+    """
+    pairing_weights = dict()
+
+    for index in range(length):
+
+        weight = weights[index]
+
+        if weight in pairing_weights:
+
+            previous_pairing_weight_index = pairing_weights[weight]
+
+            return(index, previous_pairing_weight_index)
+
+        pairing_weights[limit-weight] = index
+
     return None
